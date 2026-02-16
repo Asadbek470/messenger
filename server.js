dfc1957acc6e30f7e6dfff15f699dd1609d0766f
Asadbek470 <abdumalikovasadbek401@gmail.com>
2026-02-16 22:35:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,55 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const http = require("http");
+const { Server } = require("socket.io");
 const cors = require("cors");
 const path = require("path");
-
-const authRoutes = require("./routes/auth");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
 
-mongoose.connect("YOUR_MONGODB_CONNECTION_STRING");
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use("/api/auth", authRoutes);
+let users = {};
+let messages = [];
+
+io.on("connection", (socket) => {
+
+  socket.on("register", (userData) => {
+    users[socket.id] = userData;
+    io.emit("users", Object.values(users));
+    socket.emit("chatHistory", messages);
+  });
+
+  socket.on("message", (text) => {
+    if (!users[socket.id]) return;
+
+    const msg = {
+      id: uuidv4(),
+      sender: users[socket.id].nickname,
+      text,
+      time: new Date().toLocaleTimeString()
+    };
+
+    messages.push(msg);
+    io.emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+    io.emit("users", Object.values(users));
+  });
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("Server running on " + PORT);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+const PORT = process.env.PORT || 10000;
+
+server.listen(PORT, () => {
+  console.log("Uzbek Messenger running on " + PORT);
 });
