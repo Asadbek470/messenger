f18ef2eb01d11d2eed50ec6f2eeb6e9a146d480d
Asadbek470 <abdumalikovasadbek401@gmail.com>
2026-02-17 17:16:17 +0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  phone: { type: String, unique: true },
   firstName: String,
   lastName: String,
-  birthDate: Date,
-  about: String,
-  email: { type: String, unique: true },
-  password: String,
-  userId: { type: String, unique: true },
-  twoFactorSecret: String
+  password: String, // 2FA пароль
+  verificationCode: String,
+  isVerified: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model("User", userSchema);
